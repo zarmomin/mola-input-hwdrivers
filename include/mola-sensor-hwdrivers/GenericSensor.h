59ad9f84f8b279193d09dbf48172888950bd42a8
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-02-16 00:50:25 +0100

    first stub code

---
+++ include/mola-sensor-hwdrivers/GenericSensor.h
@@ -0,0 +1,37 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2019 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   GenericSensor.h
+ * @brief  RawDataSource from any sensor supported by mrpt-hwdrivers
+ * @author Jose Luis Blanco Claraco
+ * @date   Feb 15, 2019
+ */
+#pragma once
+
+#include <mola-kernel/RawDataSourceBase.h>
+
+namespace mola
+{
+namespace sensor_hwdrivers
+{
+/**
+ *
+ * \ingroup mola_sensor_hwdrivers_grp */
+class GenericSensor : public RawDataSourceBase
+{
+   public:
+    GenericSensor();
+    ~GenericSensor() override = default;
+
+    // See docs in base class
+    void initialize(const std::string& cfg_block) override;
+    void spinOnce() override;
+
+   private:
+};
+
+}  // namespace sensor_hwdrivers
+}  // namespace mola
---
+++ src/GenericSensor.cpp
@@ -0,0 +1,56 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2019 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   GenericSensor.cpp
+ * @brief  RawDataSource from any sensor supported by mrpt-hwdrivers
+ * @author Jose Luis Blanco Claraco
+ * @date   Feb 15, 2019
+ */
+
+/** \defgroup mola_sensor_hwdrivers_grp mola_sensor_hwdrivers_grp.
+ * RawDataSource from any sensor supported by mrpt-hwdrivers
+ *
+ *
+ */
+
+#include <mola-kernel/yaml_helpers.h>
+#include <mola-sensor-hwdrivers/GenericSensor.h>
+#include <mrpt/core/initializer.h>
+#include <yaml-cpp/yaml.h>
+
+using namespace mola;
+using namespace mola::sensor_hwdrivers;
+
+MRPT_INITIALIZER(do_register){MOLA_REGISTER_MODULE(GenericSensor)}
+
+GenericSensor::GenericSensor() = default;
+
+void GenericSensor::initialize(const std::string& cfg_block)
+{
+    using namespace std::string_literals;
+
+    MRPT_START
+    ProfilerEntry tle(profiler_, "initialize");
+
+    // Mandatory parameters:
+    auto c = YAML::Load(cfg_block);
+
+    ENSURE_YAML_ENTRY_EXISTS(c, "params");
+    auto cfg = c["params"];
+    MRPT_LOG_DEBUG_STREAM("Initializing with these params:\n" << cfg);
+
+    // YAML_LOAD_MEMBER_REQ(base_dir, std::string);
+
+    MRPT_END
+}  // end initialize()
+
+void GenericSensor::spinOnce()
+{
+    MRPT_START
+    ProfilerEntry tleg(profiler_, "spinOnce");
+
+    MRPT_END
+}
